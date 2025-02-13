d289b45b5b8099d15f9879336cffafe0847c51c0
Sovit Nayak <sovitn@uci.edu>
2025-02-12 23:51:16 -0800

    added basic google analystics tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter, Quicksand } from "next/font/google";
 import type React from "react";
 import { CartProvider } from "@/context/cart-context";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -32,15 +33,26 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="description" content="Your tab description goes here." />
         <meta property="og:title" content="Your Website Title" />
-        <meta
-          property="og:description"
-          content="Your tab description goes here."
-        />
+        <meta property="og:description" content="Your tab description goes here." />
         <meta property="og:image" content="/favicon.ico" />
+
+        {/* Google Analytics */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-92Y3KNQ0KC" />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-92Y3KNQ0KC');
+            `,
+          }}
+        />
       </head>
-      <body
-        className={`${inter.variable} ${quicksand.variable} font-sans bg-white`}
-      >
+
+      <body className={`${inter.variable} ${quicksand.variable} font-sans bg-white`}>
         <CartProvider>{children}</CartProvider>
       </body>
     </html>
